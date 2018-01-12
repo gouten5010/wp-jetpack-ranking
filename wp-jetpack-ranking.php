16c9a8d09b9c4c2b6e0f237d0fd4c00d0eb9a4bc
Takenori Okashita (GOUTEN) <gouten@5010works.com>
2018-01-12 19:13:37 +0900

    Add plugin base file.

---
+++ wp-jetpack-ranking.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WP Jetpack Ranking
+Description: This plug-in is display monthly, weekly, daily posts num and ranking using JetPack stats.
+Version: 0.0.1
+Plugin URI:
+Author: Takenori Okashita (GOUTEN)
+Author URI: https://5010works.com
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain: wp-jetpack-ranking
+*/
+
